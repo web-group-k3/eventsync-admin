79c9ffebae7f77a3c7e7a008dbd39c5d4c6b7edc
FitiavanaMihaja <hei.mickael.122@gmail.com>
2026-06-27 00:11:14 +0300

    feat(rooms): register rooms resource with full CRUD in App.tsx

--- events-admin/src/App.tsx
+++ events-admin/src/App.tsx
@@ -3,11 +3,15 @@ import { customTheme } from './theme/customTheme';
 import { EventList } from './components/events/EventList';
 import { EventCreate } from './components/events/EventCreate';
 import { EventEdit } from './components/events/EventEdit';
-import dataProvider from './providers/dataProvider'
-import { authProvider } from './providers/authProvider';
 import { EventShow } from './components/events/EventShow';
+import { RoomList } from './components/rooms/RoomList';
+import { RoomCreate } from './components/rooms/RoomCreate';
+import { RoomEdit } from './components/rooms/RoomEdit';
+import { RoomShow } from './components/rooms/RoomShow';
+import dataProvider from './providers/dataProvider';
+import { authProvider } from './providers/authProvider';
 import { Dashboard } from './components/Dashboard';
-import CustomLoginPage from "./components/customLoginPage"
+import CustomLoginPage from "./components/customLoginPage";
 
 const App = () => (
     <Admin theme={customTheme} 
@@ -22,6 +26,14 @@ const App = () => (
             edit={EventEdit}
             show={EventShow}
             options={{ label: 'Event' }}
+        />
+        <Resource 
+           name="rooms" 
+            list={RoomList}
+            create={RoomCreate}
+            edit={RoomEdit}
+            show={RoomShow}
+            options={{ label: 'Room' }}
         />
     </Admin>
 );
