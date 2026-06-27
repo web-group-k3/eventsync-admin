48433af1424672a8afae86c0788acec1ad839d97
Mahasetra <Brayanmikah@gmail.com>
2026-06-27 23:27:21 +0300

    feat: ajout resource speakers dans App.tsx

--- events-admin/src/App.tsx
+++ events-admin/src/App.tsx
@@ -2,10 +2,14 @@ import { Admin, Resource } from "react-admin";
 import { customDataProvider } from "./providers/customDataProvider";
 import { RoomList } from "./components/rooms/roomList";
 import { EventList } from "./components/events/EventList";
+import { SpeakerList } from "./components/speakers/SpeakerList";
+import { SpeakerCreate } from "./components/speakers/SpeakerCreate";
+import { SpeakerEdit } from "./components/speakers/SpeakerEdit";
 
 export const App = () => (
   <Admin dataProvider={customDataProvider}>
     <Resource name="rooms" list={RoomList} />
     <Resource name="events" list={EventList} />
+     <Resource name="speakers" list={SpeakerList} edit={SpeakerEdit} create={SpeakerCreate} />
   </Admin>
 );
